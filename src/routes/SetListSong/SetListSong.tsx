a1a610b8952a495d77552943c0d89bf6f58db65f
Andy Perlitch <andyperlitch@gmail.com>
2024-10-29 20:36:17 -0700

    swipes

--- src/routes/SetListSong/SetListSong.tsx
+++ src/routes/SetListSong/SetListSong.tsx
@@ -1,4 +1,4 @@
-import { useNavigate, useParams } from "react-router-dom";
+import { /* useNavigate,  */ useParams } from "react-router-dom";
 import "./SetListSong.css";
 import { useSetList } from "../../hooks/useSetList";
 import { useMemo, useState } from "react";
@@ -8,7 +8,7 @@ import { ControlBar } from "./ControlBar";
 import { SongViewType } from "./types";
 import { Lyrics } from "./Lyrics";
 import { SongInfo } from "./SongInfo";
-import { useSwipeable } from "react-swipeable";
+// import { useSwipeable } from "react-swipeable";
 import { SET_BREAK_TITLE } from "../../consts/songs";
 import { CenteredMessage } from "../../CenteredMessage";
 import { getPatOnTheBack } from "../../utils/getPatOnTheBack";
@@ -16,7 +16,7 @@ import { HomeButton } from "@/components/HomeButton";
 
 export function SetListSong() {
   const params = useParams();
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
   const [songView, setSongView] = useState<SongViewType>("leadsheet");
   const currentIndex = parseInt(params.songIndex || "0", 10);
   const { loading } = useSetList();
@@ -31,30 +31,31 @@ export function SetListSong() {
   const previousIndex = Math.max(0, currentIndex - 1);
   const nextIndex = Math.min(songs.length - 1, currentIndex + 1);
 
-  const swipeHandlers = useSwipeable(
-    useMemo(
-      () => ({
-        onSwipedRight: () => {
-          if (previousIndex !== currentIndex) {
-            navigate(`/sets/${params.setSlug}/${previousIndex}`);
-          }
-        },
-        onSwipedLeft: () => {
-          if (nextIndex !== currentIndex) {
-            navigate(`/sets/${params.setSlug}/${nextIndex}`);
-          }
-        },
-      }),
-      [currentIndex, navigate, nextIndex, previousIndex, params.setSlug]
-    )
-  );
+  // const swipeHandlers = useSwipeable(
+  //   useMemo(
+  //     () => ({
+  //       onSwipedRight: () => {
+  //         if (previousIndex !== currentIndex) {
+  //           navigate(`/sets/${params.setSlug}/${previousIndex}`);
+  //         }
+  //       },
+  //       onSwipedLeft: () => {
+  //         if (nextIndex !== currentIndex) {
+  //           navigate(`/sets/${params.setSlug}/${nextIndex}`);
+  //         }
+  //       },
+  //     }),
+  //     [currentIndex, navigate, nextIndex, previousIndex, params.setSlug]
+  //   )
+  // );
 
   if (loading || !songs.length || !currentSong) {
     return <div>loading...</div>;
   }
 
   return (
-    <div className="setlistRoot font-inter" {...swipeHandlers}>
+    // <div className="setlistRoot font-inter" {...swipeHandlers}>
+    <div className="setlistRoot font-inter" /* {...swipeHandlers} */>
       <HomeButton />
       <ControlBar
         previousIndex={previousIndex}
