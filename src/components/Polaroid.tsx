583cd59770fddddaf31b6e6e418dc5f410c3a836
Andy Perlitch <andyperlitch@gmail.com>
2024-10-30 09:44:22 -0700

    photo animation

--- src/components/Polaroid.tsx
+++ src/components/Polaroid.tsx
@@ -1,16 +1,32 @@
+import { cn } from "@/lib/utils";
+
 export function Polaroid({
   width = 300,
   photoUrl,
   caption,
+  className,
+  style,
 }: {
-  photoUrl: string;
+  photoUrl?: string | null;
   caption?: string;
-  width?: number;
+  width?: number | string;
+  className?: string;
+  style?: React.CSSProperties;
 }) {
   return (
-    <div className="polaroid bg-white p-4 shadow-md">
-      <img width={width} src={photoUrl} />
-      <div className="font-hand text-sm">{caption || ""}</div>
+    <div
+      className={cn(
+        `
+          polaroid rounded-sm bg-white p-4
+          shadow-[inset_0_0_10px_rgba(0,0,0,0.2)] shadow-md
+        `,
+        className
+      )}
+      style={style}
+    >
+      {photoUrl && <img style={{ width }} src={photoUrl} />}
+
+      <div className="p-4 font-hand text-5xl text-black">{caption || " "}</div>
     </div>
   );
 }
--- src/routes/stage/StagePhotoCarouselEngagement.tsx
+++ src/routes/stage/StagePhotoCarouselEngagement.tsx
@@ -1,4 +1,6 @@
 // import { CodeBlock } from "@/components/CodeBlock";
+import { Polaroid } from "@/components/Polaroid";
+import "./StagePhotoCarouselEngagement.css";
 import {
   StageEngagementFragment,
   useStageGetSubmissionQuery,
@@ -12,6 +14,10 @@ export function StagePhotoCarouselEngagement({
 }) {
   const visibleSubmissionId = engagement.viewData?.visibleSubmission;
   const [photoUrl, setPhotoUrl] = useState<string | null>(null);
+  const [caption, setCaption] = useState<string>("");
+  const [rotation, setRotation] = useState<number>(0);
+  const [scale, setScale] = useState<number>(1);
+  const [fadeClass, setFadeClass] = useState<string>("polaroid-fade-in");
   const { data /* , loading, error  */ } = useStageGetSubmissionQuery({
     skip: !visibleSubmissionId,
     variables: { id: visibleSubmissionId },
@@ -22,18 +28,27 @@ export function StagePhotoCarouselEngagement({
       const url = `https://thecasualfunk.s3.us-west-1.amazonaws.com/${data?.submission?.data?.photoUrl}`;
       const loader = new Image();
       loader.src = url;
+      setFadeClass("polaroid-fade-out");
+      setRotation(Math.round(Math.random() * 20 - 10));
+      setScale(2);
       loader.onload = () => {
         setPhotoUrl(url);
+        setCaption(data?.submission?.data?.caption || "");
+        setFadeClass("polaroid-fade-in");
+        setScale(1);
       };
     }
   }, [data]);
 
   return (
-    <div>
-      <h1>{engagement.title}</h1>
-      {/* <CodeBlock json={engagement.viewData} />
-      {visibleSubmission && <CodeBlock json={visibleSubmission} />} */}
-      {photoUrl && <img width="300" src={photoUrl} />}
+    <div className="flex h-full w-full items-center pb-12 pl-[10vw] pt-24">
+      <Polaroid
+        className={fadeClass}
+        photoUrl={photoUrl}
+        caption={caption}
+        width="30vw"
+        style={{ transform: `rotate(${rotation}deg) scale(${scale})` }}
+      />
     </div>
   );
 }
