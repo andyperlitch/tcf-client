9aded918741d9c4e730abbb0b2b9c44eee70cbd9
Andy Perlitch <andyperlitch@gmail.com>
2024-10-29 19:26:16 -0700

    fixed ts errors

--- src/routes/stage/StagePhotoCarouselEngagement.tsx
+++ src/routes/stage/StagePhotoCarouselEngagement.tsx
@@ -1,8 +1,9 @@
-import { CodeBlock } from "@/components/CodeBlock";
+// import { CodeBlock } from "@/components/CodeBlock";
 import {
   StageEngagementFragment,
   useStageGetSubmissionQuery,
 } from "@/gql/graphql";
+import { useEffect, useState } from "react";
 
 export function StagePhotoCarouselEngagement({
   engagement,
@@ -10,22 +11,29 @@ export function StagePhotoCarouselEngagement({
   engagement: StageEngagementFragment;
 }) {
   const visibleSubmissionId = engagement.viewData?.visibleSubmission;
-  const { data, loading, error } = useStageGetSubmissionQuery({
+  const [photoUrl, setPhotoUrl] = useState<string | null>(null);
+  const { data /* , loading, error  */ } = useStageGetSubmissionQuery({
     skip: !visibleSubmissionId,
     variables: { id: visibleSubmissionId },
   });
+
+  useEffect(() => {
+    if (data?.submission?.data?.photoUrl) {
+      const url = `https://thecasualfunk.s3.us-west-1.amazonaws.com/${data?.submission?.data?.photoUrl}`;
+      const loader = new Image();
+      loader.src = url;
+      loader.onload = () => {
+        setPhotoUrl(url);
+      };
+    }
+  }, [data]);
 
   return (
     <div>
       <h1>{engagement.title}</h1>
       {/* <CodeBlock json={engagement.viewData} />
       {visibleSubmission && <CodeBlock json={visibleSubmission} />} */}
-      {data?.submission?.data?.photoUrl && (
-        <img
-          width="300"
-          src={`https://thecasualfunk.s3.us-west-1.amazonaws.com/${data.submission.data.photoUrl}`}
-        />
-      )}
+      {photoUrl && <img width="300" src={photoUrl} />}
     </div>
   );
 }
