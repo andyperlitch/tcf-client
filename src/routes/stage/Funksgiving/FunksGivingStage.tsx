10401ff58e8e254d9469c832c8e48b2a6b68e577
Andy Perlitch <andyperlitch@gmail.com>
2024-10-30 22:38:27 -0700

    stage updates

--- src/routes/stage/Funksgiving/FunksGivingStage.tsx
+++ src/routes/stage/Funksgiving/FunksGivingStage.tsx
@@ -1,14 +1,9 @@
-import QRCode from "react-qr-code";
 import { useStageEvent } from "@/hooks/useStageEvent";
-import { useParamsSafe } from "@/hooks/useParamsSafe";
 import { StageActiveEngagement } from "../StageActiveEngagement";
 import { useFunksgivingBackground } from "./useFunksgivingBackground";
-import useWindowSize from "@/hooks/useWindowSize";
 
 export function FunksGivingStage() {
-  const { slug } = useParamsSafe("slug");
   const { data } = useStageEvent("funksgiving");
-  const { width } = useWindowSize();
 
   useFunksgivingBackground();
 
@@ -18,40 +13,28 @@ export function FunksGivingStage() {
         flex h-screen w-screen flex-col items-center justify-center gap-4
       `}
     >
-      <div
-        className={`
-          absolute right-4 top-4 flex h-screen flex-col items-center
-          justify-center space-x-4
-        `}
-      >
-        <div className="flex flex-col items-center">
-          <p className="text-center font-hand text-6xl text-[#894c37]">
-            Only scan this
-            <br /> if you're cool
-          </p>
-          <div className="mb-4 mt-2 text-6xl">👇</div>
-          <div className="rounded-lg bg-[#fae1aa77] p-4">
-            <QRCode
-              value={`${window.location.origin}/e/${slug}`}
-              size={width * 0.15}
-              level="L"
-              fgColor="#894c37"
-              bgColor="#fae1aa77"
-            />
-          </div>
-        </div>
+      {/* funksgiving logo */}
+      <img
+        className="absolute left-1/2 top-[2vw] -translate-x-1/2"
+        src="/funksgiving-fan-title.png"
+        style={{ width: "50vw" }}
+      />
+      {/* top right logo and text */}
+      <div className="absolute right-[3vw] top-[0.5vw]">
+        <p className="relative -left-10 font-hand text-4xl text-[#593312]">
+          ...an evening with
+        </p>
+        <img src="/funksgiving-logo.svg" style={{ width: "17vw" }} />
       </div>
+
+      {/* active engagement */}
       {data?.event?.activeEngagement ? (
         <StageActiveEngagement event={data.event} />
       ) : (
         <>
           <h1>Welcome to FunksGiving!</h1>
-          <p>
-            Scan the following QR code on your phone to be in on the action.
-          </p>
         </>
       )}
-      {/* <CodeBlock json={data} /> */}
     </div>
   );
 }
--- src/routes/stage/StagePhotoCarouselEngagement.tsx
+++ src/routes/stage/StagePhotoCarouselEngagement.tsx
@@ -6,6 +6,9 @@ import {
   useStageGetSubmissionQuery,
 } from "@/gql/graphql";
 import { useEffect, useRef, useState } from "react";
+import useWindowSize from "@/hooks/useWindowSize";
+import { useParamsSafe } from "@/hooks/useParamsSafe";
+import QRCode from "react-qr-code";
 
 interface Photo {
   photoUrl: string;
@@ -18,7 +21,7 @@ interface Photo {
   id: number;
 }
 
-const MAX_PHOTOS = 15;
+const MAX_PHOTOS = 20;
 const initialStyle = {
   opacity: 0,
   transform: "scale(3) rotate(0deg) translate(0, 0)",
@@ -29,6 +32,7 @@ export function StagePhotoCarouselEngagement({
 }: {
   engagement: StageEngagementFragment;
 }) {
+  const { slug } = useParamsSafe("slug");
   const nextPhotoId = useRef(0);
   const visibleSubmissionId = engagement.viewData?.visibleSubmission;
   const [photos, setPhotos] = useState<Photo[]>([]);
@@ -36,6 +40,7 @@ export function StagePhotoCarouselEngagement({
     skip: !visibleSubmissionId,
     variables: { id: visibleSubmissionId },
   });
+  const { width } = useWindowSize();
 
   useEffect(() => {
     if (data?.submission?.data?.photoUrl) {
@@ -51,8 +56,12 @@ export function StagePhotoCarouselEngagement({
               caption: data?.submission?.data?.caption || "",
               rotation: Math.round(Math.random() * 20 - 10),
               scale: 1,
-              translateX: Math.round(Math.random() * 100 - 50),
-              translateY: Math.round(Math.random() * 50 - 25),
+              translateX: Math.round(
+                Math.random() * width * 0.35 - width * 0.05
+              ),
+              translateY: Math.round(
+                Math.random() * width * 0.05 - width * 0.035
+              ),
               id: nextPhotoId.current++,
             },
           ];
@@ -63,24 +72,48 @@ export function StagePhotoCarouselEngagement({
         });
       };
     }
-  }, [data]);
+  }, [data, width]);
 
   return (
-    <div className="flex h-full w-full items-center pb-12 pl-[10vw] pt-24">
-      {photos.map((photo) => (
-        <Polaroid
-          key={photo.id}
-          className="absolute polaroid-fade-in"
-          photoUrl={photo.photoUrl}
-          caption={photo.caption}
-          width="30vw"
-          initialStyle={initialStyle}
-          style={{
-            opacity: 1,
-            transform: `rotate(${photo.rotation}deg) scale(${photo.scale}) translate(${photo.translateX}px, ${photo.translateY}px)`,
-          }}
-        />
-      ))}
+    <div className="flex h-full w-full items-center">
+      <div className="h-full w-1/2 pl-[10vw] pt-[25vh]">
+        {photos.map((photo) => (
+          <Polaroid
+            key={photo.id}
+            className="absolute polaroid-fade-in"
+            photoUrl={photo.photoUrl}
+            caption={photo.caption}
+            width="30vw"
+            initialStyle={initialStyle}
+            style={{
+              opacity: 1,
+              transform: `rotate(${photo.rotation}deg) scale(${photo.scale}) translate(${photo.translateX}px, ${photo.translateY}px)`,
+            }}
+          />
+        ))}
+      </div>
+      <div className="flex h-full w-1/2 flex-col items-end justify-end">
+        <div
+          className={`
+            mb-4 mr-4 flex flex-col items-center rounded-lg bg-[#fae1aa] p-4
+          `}
+        >
+          <p
+            className={`text-center font-hand text-5xl text-[#894c37]`}
+            style={{ width: `${width * 0.15}px` }}
+          >
+            Only scan this if you're cool
+          </p>
+          <QRCode
+            value={`${window.location.origin}/e/${slug}`}
+            size={width * 0.15}
+            level="L"
+            fgColor="#894c37"
+            bgColor="#fae1aa77"
+            className="mt-4"
+          />
+        </div>
+      </div>
     </div>
   );
 }
