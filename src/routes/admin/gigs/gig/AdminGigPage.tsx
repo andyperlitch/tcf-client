aac75956eabb63f4ac1891cf650e21a94125be50
Andy Perlitch <andyperlitch@gmail.com>
2025-01-31 00:10:35 -0800

    feat: CreateGigSetButton

--- src/routes/admin/gigs/gig/AdminGigPage.tsx
+++ src/routes/admin/gigs/gig/AdminGigPage.tsx
@@ -2,7 +2,8 @@ import { AdminContainer } from "@/components/AdminContainer";
 import { ErrorMessage } from "@/components/ErrorMessage";
 import { Loader } from "@/components/Loader";
 import { CrumbMeta, SimpleCrumbs } from "@/components/SimpleCrumbs";
-import { useBandGigQuery } from "@/gql/graphql";
+import { Button } from "@/components/ui/button";
+import { useBandCreateGigSetMutation, useBandGigQuery } from "@/gql/graphql";
 import { useParamsSafe } from "@/hooks/useParamsSafe";
 
 const crumbs: CrumbMeta[] = [["/admin/gigs", "Gigs"]];
@@ -22,19 +23,53 @@ export function AdminGigPage() {
     skip: !params.gigId,
   });
 
+  const gig = gigData?.gig;
+
   return (
     <AdminContainer section="gigs">
-      <div data-name="GIG_HEADER" className="flex flex-col gap-2">
-        <SimpleCrumbs crumbs={crumbs} />
-        <h1 className="text-2xl font-bold">{gigData?.gig?.name || "..."}</h1>
-      </div>
-      {gigLoading && <Loader />}
-      {gigError && <ErrorMessage error={gigError} retry={refetchGig} />}
-      {gigData?.gig && (
-        <div data-name="GIG_DETAILS">
-          <div></div>
+      <div className="flex flex-col gap-4">
+        <div data-name="GIG_HEADER" className="flex flex-col gap-2">
+          <SimpleCrumbs crumbs={crumbs} />
+          <h1 className="text-2xl font-bold">{gigData?.gig?.name || "..."}</h1>
         </div>
-      )}
+        {gigLoading && <Loader />}
+        {gigError && <ErrorMessage error={gigError} retry={refetchGig} />}
+        {gig && (
+          <div data-name="GIG_DETAILS">
+            {!gig.sets.length && (
+              <CreateGigSetButton gigId={gig.id} onSuccess={refetchGig} />
+            )}
+          </div>
+        )}
+      </div>
     </AdminContainer>
+  );
+}
+
+function CreateGigSetButton({
+  gigId,
+  onSuccess,
+}: {
+  gigId: number;
+  onSuccess: () => void;
+}) {
+  const [createGigSet, { loading }] = useBandCreateGigSetMutation();
+
+  return (
+    <Button
+      disabled={loading}
+      onClick={() =>
+        createGigSet({
+          variables: {
+            gigId,
+            data: {
+              name: "Set 1",
+            },
+          },
+        }).then(onSuccess)
+      }
+    >
+      Add Set
+    </Button>
   );
 }
