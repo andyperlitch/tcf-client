976be55d56220a064b008a2eab96eb76fb45ea2e
Andy Perlitch <andyperlitch@gmail.com>
2025-01-30 16:57:52 -0800

    feat: colorize logger

--- src/utils/createLogger.ts
+++ src/utils/createLogger.ts
@@ -1,10 +1,10 @@
 export function createLogger(name: string) {
   return {
-    log: console.log.bind(console, `[${name}]`),
-    error: console.error.bind(console, `[${name}]`),
-    warn: console.warn.bind(console, `[${name}]`),
-    info: console.info.bind(console, `[${name}]`),
-    debug: console.debug.bind(console, `[${name}]`),
-    trace: console.trace.bind(console, `[${name}]`),
+    log: console.log.bind(console, `%c[${name}]`, "color: steelblue;"),
+    error: console.error.bind(console, `%c[${name}]`, "color: crimson;"),
+    warn: console.warn.bind(console, `%c[${name}]`, "color: orange;"),
+    info: console.info.bind(console, `%c[${name}]`, "color: teal;"),
+    debug: console.debug.bind(console, `%c[${name}]`, "color: purple;"),
+    trace: console.trace.bind(console, `%c[${name}]`, "color: gray;"),
   };
 }
